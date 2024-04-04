6370a95e43007bc274aebd85563443a5c3fc17dc
maryfr77 <MARYCARMENFLORES007@outlook.com>
2024-04-04 12:03:01 -0600

    Se agrega el proyecto

---
+++ calcu/js/generadorTabla.js
@@ -0,0 +1,182 @@
+const colores = new Array(
+    "aliceblue",
+    "antiquewhite",
+    "aqua",
+    "aquamarine",
+    "azure",
+    "beige",
+    "bisque",
+    "black",
+    "blanchedalmond",
+    "blue",
+    "blueviolet",
+    "brown",
+    "burlywood",
+    "cadetblue",
+    "chartreuse",
+    "chocolate",
+    "coral",
+    "cornflowerblue",
+    "cornsilk",
+    "crimson",
+    "cyan",
+    "darkblue",
+    "darkcyan",
+    "darkgoldenrod",
+    "darkgray",
+    "darkgreen",
+    "darkgrey",
+    "darkkhaki",
+    "darkmagenta",
+    "darkolivegreen",
+    "darkorange",
+    "darkorchid",
+    "darkred",
+    "darksalmon",
+    "darkseagreen",
+    "darkslateblue",
+    "darkslategray",
+    "darkslategrey",
+    "darkturquoise",
+    "darkviolet",
+    "deeppink",
+    "deepskyblue",
+    "dimgray",
+    "dimgrey",
+    "dodgerblue",
+    "firebrick",
+    "floralwhite",
+    "forestgreen",
+    "fuchsia",
+    "gainsboro",
+    "ghostwhite",
+    "gold",
+    "goldenrod",
+    "gray",
+    "green",
+    "greenyellow",
+    "grey",
+    "honeydew",
+    "hotpink",
+    "indianred",
+    "indigo",
+    "ivory",
+    "khaki",
+    "lavender",
+    "lavenderblush",
+    "lawngreen",
+    "lemonchiffon",
+    "lightblue",
+    "lightcoral",
+    "lightcyan",
+    "lightgoldenrodyellow",
+    "lightgray",
+    "lightgreen",
+    "lightgrey",
+    "lightpink",
+    "lightsalmon",
+    "lightseagreen",
+    "lightskyblue",
+    "lightslategray",
+    "lightslategrey",
+    "lightsteelblue",
+    "lightyellow",
+    "lime",
+    "limegreen",
+    "linen",
+    "magenta",
+    "maroon",
+    "mediumaquamarine",
+    "mediumblue",
+    "mediumorchid",
+    "mediumpurple",
+    "mediumseagreen",
+    "mediumslateblue",
+    "mediumspringgreen",
+    "mediumturquoise",
+    "mediumvioletred",
+    "midnightblue",
+    "mintcream",
+    "mistyrose",
+    "moccasin",
+    "navajowhite",
+    "navy",
+    "oldlace",
+    "olive",
+    "olivedrab",
+    "orange",
+    "orangered",
+    "orchid",
+    "palegoldenrod",
+    "palegreen",
+    "paleturquoise",
+    "palevioletred",
+    "papayawhip",
+    "peachpuff",
+    "peru",
+    "pink",
+    "plum",
+    "powderblue",
+    "purple",
+    "red",
+    "rosybrown",
+    "royalblue",
+    "saddlebrown",
+    "salmon",
+    "sandybrown",
+    "seagreen",
+    "seashell",
+    "sienna",
+    "silver",
+    "skyblue",
+    "slateblue",
+    "slategray",
+    "slategrey",
+    "snow",
+    "springgreen",
+    "steelblue",
+    "tan",
+    "teal",
+    "thistle",
+    "tomato",
+    "turquoise",
+    "violet",
+    "wheat",
+    "white",
+    "whitesmoke",
+    "yellow",
+    "yellowgreen"
+);
+
+const generarTabla = () => {
+    const numFilas = document.getElementById("numFilas").value;
+    const numColumnas = document.getElementById("numColumnas").value;
+    const contenedorTabla = document.getElementById("contenedorTabla");
+
+    contenedorTabla.innerHTML = "";
+    let colorFondo, colorLetra;
+    let tabla = "<table style='border: 1px solid black;'>";
+    // var tabla = "<table class='table'>";
+    for (let k = 1; k <= numFilas; k++) {
+        tabla += "<tr>";
+        for (let o = 1; o <= numColumnas; o++) {
+            colorFondo = colores[Math.round(Math.random() * ((colores.length - 1) - 0) + 0)];
+            do {
+                colorLetra = colores[Math.round(Math.random() * ((colores.length - 1) - 0) + 0)];
+            } while (colorFondo === colorLetra);
+            console.log(colorFondo + " | " + colorLetra);
+            tabla += "<td style='border: 1px solid black; background-color: " + colorFondo + "; color: " + colorLetra + "; padding: 4px; text-align: center;'></td> ";
+        }
+        tabla += "</tr>";
+    }
+    tabla += "</table>";
+    contenedorTabla.innerHTML = tabla;
+};
+
+window.addEventListener('load', function () {
+
+    document.getElementById('botonGenerar').addEventListener('click', function () {
+        generarTabla();
+    });
+
+});
